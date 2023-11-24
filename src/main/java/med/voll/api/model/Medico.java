1ef6172a819b07f46f40b056867416fa47d3c5de
Pedro K. L <pedro.karasudani@hotmail.com>
2023-11-24 17:14:12 -0300

    Entidades JPA

---
+++ src/main/java/med/voll/api/model/Medico.java
@@ -0,0 +1,38 @@
+package med.voll.api.model;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.dto.address.Address;
+import med.voll.api.dto.doctor.Specialty;
+
+@Table(name = "medicos")
+@Entity(name = "Medicos")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Specialty specialty;
+
+    @Embedded
+    private Address address;
+}
