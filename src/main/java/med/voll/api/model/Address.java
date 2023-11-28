268f5293b374435826fe39d3caa7eb9667d6245a
Pedro K. L <pedro.karasudani@hotmail.com>
2023-11-27 21:11:49 -0300

    Criando table SQL - MIGRATION com FLYWAY

--- src/main/java/med/voll/api/model/Address.java
+++ src/main/java/med/voll/api/model/Address.java
@@ -1,9 +1,10 @@
-package med.voll.api.dto.address;
+package med.voll.api.model;
 
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import med.voll.api.dto.address.AddressData;
 
 @Embeddable
 @Getter
