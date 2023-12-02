a9ebd7443cd90ba72b7bfc891e7a9e448ca7a45d
Pedro K. L <pedro.karasudani@hotmail.com>
2023-12-01 23:46:17 -0300

    Devolvendo o codigo HTTP 201

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import jakarta.validation.Valid;
 import med.voll.api.dto.doctor.ListDateDoctors;
@@ -32,9 +33,13 @@ public class DoctorController {
 
     @PostMapping
     @Transactional
-    public ResponseEntity register(@RequestBody @Valid RegistrationDataDoctor data) {
-        doctorRepository.save(new Doctor(data));
+    public ResponseEntity register(@RequestBody @Valid RegistrationDataDoctor data, UriComponentsBuilder uriBuilder) {
+        var doctor = new Doctor(data);
+        doctorRepository.save(doctor);
 
+        var uri = uriBuilder.path("/doctors/{id}").buildAndExpand(doctor.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DetailDataDoctor(doctor));
     }
 
     @GetMapping
