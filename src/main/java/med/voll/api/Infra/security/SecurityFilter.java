8f6acfbfdf725c2f19ab22a849ac51d90fd4379f
Pedro K. L <pedro.karasudani@hotmail.com>
2024-01-11 20:33:07 -0300

    Validando Token Recebido

--- src/main/java/med/voll/api/Infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/Infra/security/SecurityFilter.java
@@ -2,6 +2,7 @@ package med.voll.api.Infra.security;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -9,14 +10,19 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.Service.TokenService;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter{
 
+  @Autowired
+  private TokenService tokenService;
+
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
     var tokenJWT = recuperarToken(request);
-    System.out.println(tokenJWT);
+    var subject = tokenService.getSubject(tokenJWT);
+
     filterChain.doFilter(request, response);
   }
 
